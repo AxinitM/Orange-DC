ef7f13cd3ec1cbda3f56a74c52c3e141dcb303a2
Andrei <139020977+AxinitM@users.noreply.github.com>
2024-09-08 23:50:23 +0300

    Update script.js

--- JS Guess the Number Game/script.js
+++ JS Guess the Number Game/script.js
@@ -2,6 +2,8 @@
 
 const buttonAgain = document.getElementById("againButton");
 const buttonCheck = document.getElementById("checkButton");
+const buttonRules = document.getElementById("rulesButton");
+
 const message = document.querySelector(".message");
 const inputNumber = document.querySelector(".numberInput");
 const questionBox = document.getElementById("questionBox");
@@ -39,6 +41,7 @@ function checkGuess() {
       gameOver = true;
       disableInput();
       buttonCheck.style.visibility = "hidden";
+      buttonRules.style.visibility = "hidden";
       jsConfetti.addConfetti({
         emojis: ["💀", "🦴", "💥", "👻", "☠️"],
         emojiSize: 50,
@@ -84,6 +87,7 @@ function checkGuess() {
     gameOver = true;
     disableInput();
     buttonCheck.style.visibility = "hidden";
+    buttonRules.style.visibility = "hidden";
   }
   // Incorrect number
   else {
@@ -114,6 +118,8 @@ function checkGuess() {
       gameOver = true;
       disableInput();
       buttonCheck.style.visibility = "hidden";
+      buttonRules.style.visibility = "hidden";
+      
     }
   }
   // New score
@@ -125,7 +131,9 @@ function checkGuess() {
 function restartGame() {
   score = 20;
   randomNumber = Math.floor(Math.random() * 20) + 1;
-  console.log(`I think that ${randomNumber} is my best idea! They will never guess it!`);
+  console.log(
+    `I think that ${randomNumber} is my best idea! They will never guess it!`
+  );
   message.textContent = "Start guessing...";
   document.querySelector(".score").textContent = score;
   buttonAgain.style.backgroundColor = "";
@@ -152,10 +160,32 @@ function enableInput() {
   buttonCheck.disabled = false;
 }
 
+// Drop up Rules
+
+function dropoutRules() {
+  alert(`Rules of the Game: "Guess My Number"
+
+  The computer randomly selects a number between 1 and 20.
+  The player's goal is to guess the number with the fewest possible attempts.
+  The player has 20 attempts. The remaining number of attempts is displayed in the "Score" section.
+  After entering a number, the player can check their guess by pressing the "Check" button or the "Enter" key.
+  After each guess, the computer will indicate whether the correct number is higher, lower, or if the guess is correct.
+  Entering numbers outside the range of 1 to 20 is considered an error and counts as an attempt.
+  If the player guesses correctly, the game will congratulate them.
+  If the player doesn't guess the number within 20 attempts, they lose the game.
+  The correct number will be displayed in the center square instead of the "?" symbol at the end of the game.
+  After the game, the player can click the "Try Again!" button to restart.
+  Have fun and enjoy the game!
+  
+  P.S. If you want to read the computer's mind, check the console.`);
+  console.log(`Why do you need rules?! Game first, rules later. `);
+}
+
 // Buttons and ENTER
 
 buttonCheck.addEventListener("click", checkGuess);
 buttonAgain.addEventListener("click", restartGame);
+buttonRules.addEventListener("click", dropoutRules);
 
 inputNumber.addEventListener("keydown", function (event) {
   if (event.key === "Enter") {
